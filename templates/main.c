c19f965c535a3635b7e8e4cdefbe8ea6700aea7b
Lucas Teske <lucas@teske.com.br>
2023-01-16 02:00:29 -0300

    Update example

--- templates/main.c
+++ templates/main.c
@@ -8,13 +8,18 @@ int main(void) {
     SystemClock_Config_HSE();
     Delay_Init();
     USART_Init();
-    printf("USART Init\r\n");
+    SYSCTRL->CG_CTRL1 = SYSCTRL_APBPeriph_ALL;
+    SYSCTRL->CG_CTRL2 = SYSCTRL_AHBPeriph_ALL;
 
     GPIO_InitTypeDef gpio;
     gpio.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15;
     gpio.GPIO_Mode = GPIO_Mode_Out_PP;
     gpio.GPIO_Remap = GPIO_Remap_1;
     GPIO_Init(GPIOD, &gpio);
+
+    Delay_ms(2000);
+    printf("USART Init\r\n");
+    printf("CHIP ID: %04x\r\n", SYSCTRL->CHIP_ID);
 
     while (1) {
         GPIO_ResetBits(GPIOD, GPIO_Pin_14 | GPIO_Pin_15);
